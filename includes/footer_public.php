19cab99c6fbfbcbe519ba3eae6eea4f090479541
Caio Vinícius <caio.work@outlook.com>
2025-11-03 23:20:47 -0300

    V1: Ajuste no footer da tela de login

--- includes/footer_public.php
+++ includes/footer_public.php
@@ -13,7 +13,7 @@
         </div>
         <div class="row gy-3">
             <div class="col-12 d-flex justify-content-center align-items-center gap-2">
-                <div class="footer-version text-muted">v1.4</div>
+                <div class="footer-version text-muted">v1</div>
                 <a href="https://www.instagram.com/dynamic.motion.century" class="btn btn-outline-secondary btn-sm rounded-circle social-btn" aria-label="Instagram"><i class="bi bi-instagram"></i></a>
                 <a href="https://twitter.com/dynamicMcentury" class="btn btn-outline-secondary btn-sm rounded-circle social-btn" aria-label="twitter"><i class="bi bi-twitter"></i></a>
             </div>
